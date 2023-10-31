8208173c3f8060b2381b47e907fc85908f755032
AmmosovPavel <ammosov.pavel.a@gmail.com>
2023-10-31 23:30:09 +0300

    Homework2.1

--- src/test/java/Apples.java
+++ src/test/java/Apples.java
@@ -1,11 +1,11 @@
 public class Apples {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 6;
+        final int JABLOK_Y_BURATINO = 10;
+        final int KARABAS_VZYAL = 6;
 
-        int z = x - y;
-        System.out.println("У Буратино было " + x + " яблок" );
-        System.out.println("Карабас забрал " + y + " яблок");
-        System.out.println("У Буратино осталось " + z + " яблока");
+        final int OSTATOK_BURATINO = JABLOK_Y_BURATINO - KARABAS_VZYAL;
+        System.out.println("У Буратино было " + JABLOK_Y_BURATINO + " яблок" );
+        System.out.println("Карабас забрал " + KARABAS_VZYAL + " яблок");
+        System.out.println("У Буратино осталось " + OSTATOK_BURATINO + " яблока");
     }
 }
