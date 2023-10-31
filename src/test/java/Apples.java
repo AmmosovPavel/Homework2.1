d4e130bfaf284a4010cc293556873f75789dd531
AmmosovPavel <ammosov.pavel.a@gmail.com>
2023-10-31 23:21:10 +0300

    Homework2.1

---
+++ src/test/java/Apples.java
@@ -0,0 +1,11 @@
+public class Apples {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 6;
+
+        int z = x - y;
+        System.out.println("У Буратино было " + x + " яблок" );
+        System.out.println("Карабас забрал " + y + " яблок");
+        System.out.println("У Буратино осталось " + z + " яблока");
+    }
+}
